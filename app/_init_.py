36f7d82965177a782b522b2086ffc427bbec2a43
ghofrane333 <ghofrane.boughalmi@gmail.com>
2024-09-24 19:24:22 +0200

    Update _init_.py

--- app/_init_.py
+++ app/_init_.py
@@ -3,12 +3,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     
-    # Configuration de l'application (exemple de configuration)
+    # Configuration de l'application (si nécessaire)
     app.config.from_object('config.Config')
     
     # Importation des routes
     from . import routes
-    app.register_blueprint(main.bp)
+    app.register_blueprint(routes.bp)
 
     return app
 
