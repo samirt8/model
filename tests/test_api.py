e721d03eae9d7e21cb203cdff3356d636af6f90c
ghofrane333 <ghofrane.boughalmi@gmail.com>
2024-09-25 16:51:30 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,11 @@
 import pytest
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../api')))
 
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api.app import app  # Importez l'application Flask
+
+
 
 @pytest.fixture
 def client():
