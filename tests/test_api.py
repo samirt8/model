f942fa219444dac3320bd18d8fe1e14465f3fca6
ghofrane333 <ghofrane.boughalmi@gmail.com>
2024-09-25 17:30:00 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from api.app import app  # Importez l'application Flask
+from app import app  # Importez l'application Flask
 
 
 
