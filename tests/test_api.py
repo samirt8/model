7a679886f43f3afd4383c1b9309f6151ff4a5896
ghofrane333 <ghofrane.boughalmi@gmail.com>
2024-09-25 16:36:15 +0200

    modification des fichier

---
+++ tests/test_api.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../api')))
+
+
+from api.app import app  # Importez l'application Flask
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
